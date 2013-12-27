2872f408541003abca7fc9cefe1eeadd5b1173d7
slava hatnuke <slava.hatnuke@gmail.com>
2013-12-28 01:04:21 +0200

    wip

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl -sSL https://github.com/slavahatnuke/vagranty-symfony2//tarball/master | tar -zx --strip-components 1
+curl -sSL https://github.com/slavahatnuke/vagranty-symfony2/tarball/master | tar -zx --strip-components 1
 
 rm .gitignore
 rm install.sh
